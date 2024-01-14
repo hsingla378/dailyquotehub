267ac7f27eaee961b24e10b02d0ac0554e35f3b7
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-01-15 00:31:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Authors from "./components/Authors";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
