880af138684a7f16f3b3161f2b6471701d627015
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-01-23 14:25:35 +0530

    resolve get quote by slug bug

--- backend/models/quote.js
+++ backend/models/quote.js
@@ -1,29 +1,3 @@
-// const mongoose = require("mongoose");
-// var slug = require("mongoose-slug-generator");
-// mongoose.plugin(slug);
-
-// const quoteSchema = new mongoose.Schema({
-//   title: String,
-//   description: String,
-//   categories: [String],
-//   thumbnail: String,
-//   author: {
-//     name: { type: String, lowercase: true },
-//     designation: String,
-//     description: String,
-//     avatar: String,
-//   },
-//   book: {
-//     name: String,
-//     image: String,
-//     amazonLink: String,
-//   },
-//   slug: { type: String, slug: "title"}
-// });
-
-// quoteSchema.set('timestamps', true);
-// module.exports = mongoose.model("Quote", quoteSchema);
-
 const mongoose = require("mongoose");
 
 const quoteSchema = new mongoose.Schema({
--- backend/routes/quoteRoutes.js
+++ backend/routes/quoteRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 router.get("/", quoteController.getAllQuotes);
 router.get("/search", quoteController.searchQuotes);
 
-router.get("/:id", quoteController.getQuoteBySlug);
+router.get("/:slug", quoteController.getQuoteBySlug);
 router.post("/", authMiddleware, quoteController.createQuote);
 router.put("/:id", authMiddleware, quoteController.updateQuote);
 router.delete("/:id", authMiddleware, quoteController.deleteQuote);
